4deae602ec90fe6746171d90a85fd86c4c2a88cb
Guilherme Max Pereira <99480109+Guimax18@users.noreply.github.com>
2024-07-24 22:59:45 -0300

    Add files via upload

---
+++ port-new-main/js/script.js
@@ -0,0 +1,75 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 4,
+  spaceBetween: 15,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+
+const accordion_item = document.querySelectorAll(".accordion_item");
+
+accordion_item.forEach((item) => {
+  const accordion_header_item = item.querySelector(".accordion_header");
+
+  accordion_header_item.addEventListener("click", () => {
+    const accordion_content_item = item.querySelector(".accordion_content");
+
+    const item_actived = document.querySelector(".active");
+
+    VerifyActive(item, accordion_content_item, item_actived);
+  });
+});
+
+function VerifyActive(item, content, content_actived) {
+  const icon_item = item.querySelector(".icon-accordion");
+  const icon_item_active = document.querySelectorAll(".icon");
+
+  icon_item_active.forEach((item) => (item.innerHTML = "+"));
+
+  if (content_actived) {
+    content_actived.style.height = 0;
+    content_actived.classList.remove("active");
+  }
+
+  if (content !== content_actived) {
+    icon_item.innerHTML = "-";
+    content.classList.add("active");
+    content.style.height = content.scrollHeight + 10 + "px";
+  }
+}
+
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
